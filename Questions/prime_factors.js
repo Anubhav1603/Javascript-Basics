abeaf3bdd2c82fa3f2968a90c2ca0064a1157ce7
Anubhav1603 <anubhavsinha110@gmail.com>
2021-12-28 23:36:42 +0530

    changed some code logic

--- Questions/prime_factors.js
+++ Questions/prime_factors.js
@@ -1,15 +1,37 @@
 //Find all prime factors of a number
 
+
+function verifyPrime(num){
+    let prime=0;
+    for(let i=0;i<=num;i++){
+        if(num%i==0){
+            prime += 1;
+        }
+    }
+    if (prime==2){
+        return num
+    }
+    else {
+        return 0
+    }
+}
+
 function findPrime(num){
     let factors = []; //factors array to store factors
+    let prime=[]
 
     for(let i=2;i<num;i++){
         if(num%i==0){
             factors.push(i)
         }
     }
-
-    console.log(factors)
+    for(let j in factors){
+        let c = verifyPrime(factors[j])
+        if(c != 0){
+            prime.push(c)
+        }
+    }
+    console.log(prime)
 }
 
-findPrime(55)
+findPrime(758575)
